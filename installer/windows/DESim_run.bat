6b98c32b1210c75ce303b0c9bca4a976d6534313
fpgacademy <74740408+fpgacademy@users.noreply.github.com>
2022-04-05 16:47:56 -0400

    Minor update to DESim_run.bat
    Using batch file comments instead of REM

--- installer/windows/DESim_run.bat
+++ installer/windows/DESim_run.bat
@@ -1,9 +1,19 @@
-REM Copyright (c) 2020 FPGAcademy
-REM Please see license at https://github.com/fpgacademy/DESim
+:: Copyright (c) 2020 FPGAcademy
+:: Please see license at https://github.com/fpgacademy/DESim
+
+:: If ModelSim/Questa is not part of your path, please use the command line 
+:: argument '--simulator-path'. Replace <SimulatorPath> with the path to 
+:: modelsim/questa on your computer, such as
+::   a) C:\intelFPGA\19.1\modelsim_ase\win32aloem
+::   b) C:\intelFPGA\21.1\questa_fse\win64
+::
+:: If a license file is need by ModelSim/Questa and the LM_LICENSE_FILE 
+:: environment varible does not exist. Please use the command line argument 
+:: '--license-file-path'. Replace <LicenseFilePath> with the path to the license
+:: file on your computer.
 
 start java\bin\javaw.exe -m DESim/GUI.Main
-REM If ModelSim is not part of your path. Please 
-REM 1) comment out the line above
-REM 2) uncomment the line below
-REM 3) replace <YourModelSimPath> with the path to modelsim on your computer, such as C:\intelFPGA\19.1\modelsim_ae\win32aloem
-REM start java\bin\javaw.exe -m DESim/GUI.Main --modelsim-path=<YourModelSimPath>
+:: start java\bin\javaw.exe -m DESim/GUI.Main --simulator-path=<SimulatorPath>
+:: start java\bin\javaw.exe -m DESim/GUI.Main --license-file-path=<LicenseFilePath>
+:: start java\bin\javaw.exe -m DESim/GUI.Main --simulator-path=<SimulatorPath> --license-file-path=<LicenseFilePath>
+
