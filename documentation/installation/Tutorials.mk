8c8d5563e1373977e13ae0dbe2f478eb0408a0e6
fpgacademy <74740408+fpgacademy@users.noreply.github.com>
2022-04-20 11:37:01 -0400

    Updated DESim Installation Guide for 21.1 release

---
+++ documentation/installation/Tutorials.mk
@@ -0,0 +1,7 @@
+# This Makefile compiles the tutorial in this folder
+
+TUTROOT = ../../../Tutorials
+TEXFILES = DESim_Install_Guide.tex
+DESIGNFILESZIP = 
+
+include $(TUTROOT)/Common/Scripts/Makefile.common 
