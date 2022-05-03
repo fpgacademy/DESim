b2e87705761cd9642ad662134387c7d53b050b4a
fpgacademy <74740408+fpgacademy@users.noreply.github.com>
2022-05-03 16:10:23 -0400

    Updates to the DESim Tutorial for 21.1 release
    Added info for Questa, VHDL, and Linux.
    Using the Tutorial make scripts
    Removing unused figures

--- demos/display/Display.v
+++ demos/display/Display.v
@@ -12,8 +12,8 @@ module Display (CLOCK, RESETn, HEX0, LEDR);
     output reg  [ 6: 0] HEX0;
     output wire [ 9: 0] LEDR;
 
-    parameter A = 8'd65, b = 8'd98, C = 8'd67, d = 8'd100, E = 8'd69, F = 8'd70, 
-        g = 8'd103, h = 8'd104;
+    parameter A = 8'd65, b = 8'd98, C = 8'd67, d = 8'd100, E = 8'd69, 
+		F = 8'd70, g = 8'd103, h = 8'd104;
 
 	wire        [ 2: 0] count;
     wire        [ 7: 0] char;
---
+++ documentation/tutorial/Verilog.mk
@@ -0,0 +1,8 @@
+# This Makefile compiles the tutorial in this folder
+
+TUTROOT = ../../../Tutorials
+TEXFILES = DESim_Tutorial_Verilog.tex
+TEXSOURCEFILES = DESim_Tutorial.tex
+DESIGNFILESZIP = 
+
+include $(TUTROOT)/Common/Scripts/Makefile.common 
