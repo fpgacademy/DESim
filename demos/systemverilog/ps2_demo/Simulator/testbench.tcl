138de3844370165e96c97fc67ddad036a5482d28
FPGAcademy <74740408+fpgacademy@users.noreply.github.com>
2025-03-10 17:55:24 -0400

    added +acc
    Needed to add _acc to Simulation folder for ps2_demo, so that Questa would show waveforms.

--- demos/systemverilog/ps2_demo/Simulator/testbench.tcl
+++ demos/systemverilog/ps2_demo/Simulator/testbench.tcl
@@ -9,7 +9,7 @@ vlog ../tb/PS2_keyboard.v
 # compile the testbench
 vlog testbench.v
 # start the Simulator, including some libraries that may be needed
-vsim work.testbench -Lf 220model -Lf altera_mf_ver -Lf verilog
+vsim -voptargs=+acc work.testbench -Lf 220model -Lf altera_mf_ver -Lf verilog
 # show waveforms specified in wave.do
 do wave.do
 # advance the simulation the desired amount of time
